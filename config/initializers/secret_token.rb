3883f2abc36181332a6cbbe77d65aa86b62bf931
Shannon Rush <shannonmrush@gmail.com>
2014-03-28 09:33:57 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rushdevo::Application.config.secret_token = 'd4b556e0376b110b448339c8baf7541d3afc595e43ee7100978bfbca8fdc99df8914967745679a805052debf1cc43957f27d9fd4b19ad45746c86f2f014823ef'
